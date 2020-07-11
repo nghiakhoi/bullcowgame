e853d9d1526891fcfdcdc8febed6ef9644757bfa
nghiakhoi <hnghiakhoi3@gmail.com>
2020-07-11 23:34:28 +0700

    update hiddenword

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -7,12 +7,12 @@ void UBullCowCartridge::BeginPlay() // When the game starts
     PrintLine(TEXT("Chao mung toi game Trau Bo Dai Chien"));
     PrintLine(TEXT("Doan 4 ki tu")); // so co the thay doi
     PrintLine(TEXT("Nhan Enter de tiep tuc..."));
+    HiddenWord = "cake";
 }
 
 void UBullCowCartridge::OnInput(const FString &Input) // When the player hits enter
 {
     ClearScreen();
-    FString HiddenWord = "cake"; //dua no ra khoi hàm này
     if (Input == HiddenWord)
     {
         PrintLine(TEXT("Win!"));
