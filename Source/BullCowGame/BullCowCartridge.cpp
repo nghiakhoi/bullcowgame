9ba5068ec9d4f4b17197a8a65df4b83489a38313
nghiakhoi <hnghiakhoi3@gmail.com>
2020-08-09 19:40:48 +0700

    update new

--- Source/BullCowGame/BullCowCartridge.cpp
+++ Source/BullCowGame/BullCowCartridge.cpp
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+// Fill out your copyright notice in the Description page of Project Settingss.
 #include "BullCowCartridge.h"
 
 void UBullCowCartridge::BeginPlay() // When the game starts
